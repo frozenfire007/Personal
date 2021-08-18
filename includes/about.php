fc1bf5720745abe5a4e2199aadfc26c40c3a790b
Ranjan Lamichhane <lamichhaneranjan0@gmail.com>
2021-08-18 11:30:27 +0545

    Creating personal website

---
+++ includes/about.php
@@ -0,0 +1,47 @@
+ <!-- ======= About Section ======= -->
+  <section id="about" class="about">
+
+    <!-- ======= About Me ======= -->
+    <div class="about-me container">
+
+      <div class="section-title">
+        <h2>About</h2>
+        <p>Learn more about me</p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4" data-aos="fade-right">
+          <img src="assets/img/me.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+          <h3>UI/UX &amp; Graphic Designer</h3>
+         <!--  <p class="fst-italic">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+            magna aliqua.
+          </p> -->
+          <div class="row">
+            <div class="col-lg-6">
+              <ul>
+                <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>19 Nov 1999</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>lamichhaneranjan0@gmail.com</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+977 9861631229</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Changunarayan -7, Bhaktapur</span></li>
+              </ul>
+            </div>
+            <div class="col-lg-6">
+              <ul>
+                <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>21</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Bachelors (Running)</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>PhEmailone:</strong> <span>lamichhaneranjan0@gmail.com</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Freelancing:</strong> <span>Available</span></li>
+              </ul>
+            </div>
+          </div>
+          <!-- <p>
+            Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.
+            Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.
+          </p> -->
+        </div>
+      </div>
+
+    </div><!-- End About Me -->
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+include 'includes/load.php'; 
+include 'includes/header.php'; 
+include 'includes/about.php';
+include 'includes/abtcount.php';
+include 'includes/abtskills.php';
+include 'includes/abtinterest.php';
+include 'includes/footer.php';
+ ?>
